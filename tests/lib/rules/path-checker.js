c773169b1e2e6a1029ab70f15fb0298d3b552573
Nurlybek <nurlybekqwerty@gmail.com>
2023-11-13 17:32:55 +0600

    eslint plugin

---
+++ tests/lib/rules/path-checker.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview fsd relative path
+ * @author nur
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/path-checker"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("path-checker", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      code: "connection lost",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
+    },
+  ],
+});
